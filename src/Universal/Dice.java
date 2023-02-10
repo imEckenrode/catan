6fc19e5f63de331c90e9c3ee6c557b6f09cb8ad3
imEckenrode <imeck121@gmail.com>
2023-02-09 19:13:06 -0500

    Implemented Dice RNG with Java.Random

--- src/Universal/Dice.java
+++ src/Universal/Dice.java
@@ -1,5 +1,18 @@
 package Universal;
 
+import java.util.Random;
+
 public class Dice {
-    //This could even be a method within Main.Main/Catan, but it also needs a GUI.GUI representation so...
+    //This could even be a method within Main/Catan, but it also needs a GUI representation so...
+
+    Random rng;
+
+    public Dice() {
+        rng = new Random();
+    }
+
+    public int rollDice(){
+        return rng.nextInt(6)+rng.nextInt(6)+2; //nextInt returns a number between 0 and bound-1, so add 1 to each die roll
+    }
+
 }
