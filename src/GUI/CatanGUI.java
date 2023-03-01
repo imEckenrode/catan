016326c67a33fc1853e9793d2263c5b707c63834
tj32p <tjt5709@psu.edu>
2023-02-28 19:16:05 -0500

    hello good sir, in this set of changes to our Catan project I have made changes to allow a loop for connecting all the edges for all the hexagons. thank you, your welcome.

---
+++ src/GUI/CatanGUI.java
@@ -0,0 +1,22 @@
+package GUI;
+
+import javax.swing.*;
+
+public class CatanGUI {
+    private JPanel OtherHandsPanel;
+    private JPanel Hand1Panel;
+    private JPanel Hand2Panel;
+    private JPanel Hand3Panel;
+    private JPanel BoardPanel;
+    private JPanel CurrentHandPanel;
+    private JPanel BuildingCardPanel;
+    private JPanel PortsPanel;
+    private JPanel ActionsPanel;
+    private JPanel CatanPanel;
+    private JPanel FourForOne;
+    private JPanel ToDoAdd6;
+
+    private void createUIComponents() {
+        // TODO: place custom component creation code here
+    }
+}
