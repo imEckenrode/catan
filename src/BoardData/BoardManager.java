51a5c4bdaca6734c859c98f4187306ddff426ddb
imEckenrode <imeck121@gmail.com>
2023-03-07 16:40:24 -0500

    Added buildCatanBoard method to abstract some logic, removed PlaceableItems reference

--- src/BoardData/BoardManager.java
+++ src/BoardData/BoardManager.java
@@ -21,17 +21,21 @@ public class BoardManager {
         //May need to reverse order of tilings so we can pop out as we go
 
     public BoardManager() {
-        Random rng = new Random();
-        ArrayList<Catan.Resource> resources = new ArrayList<>(Arrays.asList(DESERT,WOOD,WOOD,WOOD,WOOD,GRAIN,GRAIN,GRAIN,GRAIN,WOOL,WOOL,WOOL,WOOL,CLAY,CLAY,CLAY,ORE,ORE,ORE));
-        ArrayList<Integer> numberList = new ArrayList<>(Arrays.asList(5, 2, 6, 3, 8, 10, 9, 12, 11, 4, 8, 10, 9, 4, 5, 6, 3, 11));
-
         //Initialize the 2d ArrayList
         numberToTile = new ArrayList<>();
         for(int i = 0; i<13;i++){
             numberToTile.add(new ArrayList<>());
         }
 
+        buildCatanBoard();
+    }
+
+    public void buildCatanBoard(){
+        Random rng = new Random();
+        ArrayList<Catan.Resource> resources = new ArrayList<>(Arrays.asList(DESERT,WOOD,WOOD,WOOD,WOOD,GRAIN,GRAIN,GRAIN,GRAIN,WOOL,WOOL,WOOL,WOOL,CLAY,CLAY,CLAY,ORE,ORE,ORE));
+        ArrayList<Integer> numberList = new ArrayList<>(Arrays.asList(5, 2, 6, 3, 8, 10, 9, 12, 11, 4, 8, 10, 9, 4, 5, 6, 3, 11));
         hMap = new Hexagon[5][7];
+
         Catan.Resource resource = null;
         for(int j=0; j<5; j++){   //Column (maxIndex is made by j+2)
             for(int i=max(j-2,0); i<=min(j+2,4); i++){
@@ -41,19 +45,19 @@ public class BoardManager {
         }
         //loops through the middle circle of hexes
         ArrayList<Integer> indices = new ArrayList<>(Arrays.asList(2,1,3,2,3,3,2,3,1,2,1,1));
-       for (int temp1 = 0; temp1<12; temp1+=2) {
-           int j = indices.get(temp1);
-           int i = indices.get(temp1 + 1);
+        for (int temp1 = 0; temp1<12; temp1+=2) {
+            int j = indices.get(temp1);
+            int i = indices.get(temp1 + 1);
 
             //Division by 2 passed as an integer should automatically floor for positive numbers
-           hMap[j][i].radius1Attachment(temp1/2, new Hexagon[]{hMap[j - 1][i], hMap[j][i + 1],
-                   hMap[j + 1][i + 1], hMap[j + 1][i],
-                   hMap[j][i - 1], hMap[j - 1][i - 1]});
+            hMap[j][i].radius1Attachment(temp1/2, new Hexagon[]{hMap[j - 1][i], hMap[j][i + 1],
+                    hMap[j + 1][i + 1], hMap[j + 1][i],
+                    hMap[j][i - 1], hMap[j - 1][i - 1]});
 
-          //System.out.println(hMap[j + 1][i + 1].edges[5]);
-           //System.out.println(hMap[j][i].edges[2]);
-       }
-            //}
+            //System.out.println(hMap[j + 1][i + 1].edges[5]);
+            //System.out.println(hMap[j][i].edges[2]);
+        }
+        //}
         //}
 
         //TODO: Ports and Numbering (do them at the same time)
@@ -61,17 +65,17 @@ public class BoardManager {
 
         Hexagon nextHex = hMap[0][0];
         int dir = 1;
-         while(!numberList.isEmpty()){
-             if(nextHex.resource != DESERT){
+        while(!numberList.isEmpty()) {
+            if (nextHex.resource != DESERT) {
                 int num = numberList.remove(0);
                 nextHex.setTokenNum(num);
                 numberToTile.get(num).add(nextHex);
-             }
-             try {
-                 nextHex = nextHex.getNextTokenlessHex(dir);
-             }catch(IndexOutOfBoundsException e){   //Once there are no more to number, we are done
-                 break;
-             }
-         }
+            }
+            try {
+                nextHex = nextHex.getNextTokenlessHex(dir);
+            } catch (IndexOutOfBoundsException e) {   //Once there are no more to number, we are done
+                break;
+            }
+        }
     }
 }
--- src/Player/Recipe.java
+++ src/Player/Recipe.java
@@ -6,6 +6,6 @@ import java.util.ArrayList;
 
 public class Recipe {
     //The various recipes in the game and their corresponding items
-    PlaceableItems associatedItem;
+    //PlaceableItems associatedItem;
     ArrayList<Catan.Resource> rawMaterials;
 }
